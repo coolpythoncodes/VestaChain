33f79b9fc750629e385f636d7a76be26d1f03a52
coolpythoncodes@gmail.com <coolpythoncodes@gmail.com>
2024-08-18 04:10:32 +0100

    chore: dev setup and wrote token smart contract

---
+++ src/Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) Ownable(msg.sender) {
+        mint(initialSupply);
+    }
+
+    function mint(uint supply) public onlyOwner {
+                _mint(msg.sender, supply * 10 ** uint256(decimals()));
+
+    }
+}
